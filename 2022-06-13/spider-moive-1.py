e09ffd789e90e62488182fb526c3d5d05fa54070
-_- <429478948@qq.com>
2022-06-22 17:48:32 +0800

    2022-6-22

--- 2022-06-13/spider-moive-1.py
+++ 2022-06-13/spider-moive-1.py
@@ -17,15 +17,15 @@ soup = BeautifulSoup(content, 'lxml')
 # info = soup.find_all(name='div', attrs={'class': 'comment-list'})[0].find_all('li')
 info = soup.select('.comment-list>ul li')
 
-# for li in info:
-#     print('*' * 100)
-#     title = li.select('li>a')[0].attrs['title']
-#     detail_page_link = li.select('li>a')[0].attrs['href']
-#     img = li.select('li>a>img')[0].attrs['src']
-#     abstract_text = li.select('li>.list-txt>.txt-abstract')[0].text
-#
-#     print(title, detail_page_link, img, abstract_text)
-#     print('#' * 100)
+for li in info:
+    print('*' * 100)
+    title = li.select('li>a')[0].attrs['title']
+    detail_page_link = li.select('li>a')[0].attrs['href']
+    img = li.select('li>a>img')[0].attrs['src']
+    abstract_text = li.select('li>.list-txt>.txt-abstract')[0].text
+
+    print(title, detail_page_link, img, abstract_text)
+    print('#' * 100)
 
 
 url = 'https://www.1905.com/coll/newslist/pingluninfo_2.json'
