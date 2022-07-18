24112be5bf7bc6888714763d21cae3148def5777
-_- <429478948@qq.com>
2022-07-18 23:07:11 +0800

    2022-07-18

--- 2022-06-28/1.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Time : 2022/7/18 22:44 
-# @Author : chen.zhang 
-# @File : 1.py
-aa =['http://as.fang.ke.com/loupan/pg1/', 'http://as.fang.ke.com/loupan/pg2/']
-
-print(aa[0:1])
-print(aa[1:])
